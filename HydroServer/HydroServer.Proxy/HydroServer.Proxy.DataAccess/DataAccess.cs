f5dd6306f17da1f72209bc4b80b8eae0a8b84537
bendmorris_cp <SND\bendmorris_cp>
2010-06-24 16:36:00 +0000

    Added HydroServer.Proxy folder

---
+++ HydroServer/HydroServer.Proxy/HydroServer.Proxy.DataAccess/DataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HydroServer.Proxy;
+
+namespace HydroServer.Proxy
+{
+    public class DataAccess : IDisposable, IDataAccess
+    {
+        private LINQDataContext db { get; set; }
+
+        public DataAccess()
+        {
+            db = new LINQDataContext();
+        }
+
+        public List<ODMDatabase> ODMDatabases()
+        {
+            return db.ODMDatabases.ToList();
+        }
+
+        public List<MapServer> MapServers()
+        {
+            return db.MapServers.ToList();
+        }
+
+        public List<Region> Regions()
+        {
+            return db.Regions.ToList();
+        }
+
+        public void Dispose() 
+        {
+            db.Dispose();
+        }
+    }
+}
---
+++ HydroServer/HydroServer.Proxy/HydroServer.Proxy.DataAccess/IDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HydroServer.Proxy;
+
+namespace HydroServer.Proxy
+{
+    interface IDataAccess
+    {
+        List<ODMDatabase> ODMDatabases();
+        List<MapServer> MapServers();
+        List<Region> Regions();
+    }
+}
