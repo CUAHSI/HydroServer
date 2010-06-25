8c0e1db1c68ce819a04a49af682219bab40808d2
sreeder_cp <SND\sreeder_cp>
2010-06-25 20:36:14 +0000

    HydroServer Capabilities Database Configuration Tool.Fix bug when adding database to table

--- HydroServer/HISServerCapabilitiesConfig/CapabilitiesConfig/SelectDatabaseServer.vb
+++ HydroServer/HISServerCapabilitiesConfig/CapabilitiesConfig/SelectDatabaseServer.vb
@@ -10,39 +10,20 @@ Public Class SelectDatabaseServer
         ' Add any initialization after the InitializeComponent() call.
         icewaterConnection = icewater
     End Sub
-    Private Sub SelectDatabaseServer_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-        Dim connection As New Microsoft.SqlServer.Management.Common.ServerConnection(txtServers.Text)
-        Dim server As New Microsoft.SqlServer.Management.Smo.Server(connection)
-        server.Initialize()
-        usedDatabases = GetUsedDatabases(txtServers.Text)
-    End Sub
-
-    Private Function GetUsedDatabases(ByVal server As String) As Specialized.StringCollection
-        Dim Databases As New Specialized.StringCollection
-        Dim adapter As New SqlClient.SqlDataAdapter("SELECT DatabaseName FROM ODMDatabases WHERE ServerAddress LIKE '" & server & "'", IcewaterConnection)
-        Dim Table As New DataTable
-        adapter.Fill(Table)
-
-        For Each row As DataRow In Table.Rows
-            Databases.Add(row.Item("DatabaseName"))
-        Next
-        Return Databases
-    End Function
 
     Private Sub btnApply_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnApply.Click
         Dim canceled As Boolean = False
         Dim used As New List(Of String)
         'For Each usedItem As String In txtDBName.Text
-        If (Not usedDatabases.Contains(txtDBName.Text)) Then
-            Dim diag As New AddDatabase(icewaterConnection, txtServers.Text, txtDBName.Text, txtUser.Text, txtPWD.Text)
-            If diag.ShowDialog(Me) = Windows.Forms.DialogResult.OK Then
-                used.Add(txtDBName.Text)
-            Else
-                canceled = True
-            End If
+
+        Dim diag As New AddDatabase(icewaterConnection, txtServers.Text, txtDBName.Text, txtUser.Text, txtPWD.Text)
+        If diag.ShowDialog(Me) = Windows.Forms.DialogResult.OK Then
+            used.Add(txtDBName.Text)
+        Else
+            canceled = True
         End If
-        
+
+
         If canceled Then
             MsgBox("Commit changes Canceled", MsgBoxStyle.Information)
         Else
@@ -55,4 +36,5 @@ Public Class SelectDatabaseServer
         Me.Close()
     End Sub
 
+    
 End Class
