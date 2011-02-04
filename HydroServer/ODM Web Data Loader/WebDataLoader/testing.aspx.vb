6195b0530dfa64e110a80a4b88bbcbd05de9f5c9
edward0078_cp <SND\edward0078_cp>
2011-02-04 19:45:43 +0000

    Web data Loader update

--- HydroServer/ODM Web Data Loader/WebDataLoader/testing.aspx.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Partial Class testing
-    Inherits System.Web.UI.Page
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Response.Redirect("WebDataLoader.aspx")
-    End Sub
-
-End Class
