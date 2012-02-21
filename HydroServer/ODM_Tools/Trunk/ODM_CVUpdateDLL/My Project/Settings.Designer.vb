056b409b2e1c1ab5f5c250cc5fdb76e39c3e7ea2
horsburgh_cp <SND\horsburgh_cp>
2012-02-21 23:04:00 +0000

    Updated reference to ODM CV web service.

--- HydroServer/ODM_Tools/Trunk/ODM_CVUpdateDLL/My Project/Settings.Designer.vb
+++ HydroServer/ODM_Tools/Trunk/ODM_CVUpdateDLL/My Project/Settings.Designer.vb
@@ -57,7 +57,7 @@ Namespace My
         <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
          Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
          Global.System.Configuration.SpecialSettingAttribute(Global.System.Configuration.SpecialSetting.WebServiceUrl),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("http://localhost:20947/ODMCV_1_1/ODMCV_1_1.asmx")>  _
+         Global.System.Configuration.DefaultSettingValueAttribute("http://his.cuahsi.org/ODMCV_1_1/ODMCV_1_1.asmx")>  _
         Public ReadOnly Property CVUpdate_ODMCV() As String
             Get
                 Return CType(Me("CVUpdate_ODMCV"),String)
