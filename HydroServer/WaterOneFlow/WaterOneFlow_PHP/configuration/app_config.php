6c03031f65a8f88c478276fb8a9dd9fb07aac5a9
jirikadlec2_cp <SND\jirikadlec2_cp>
2012-05-29 16:58:01 +0000

    changes in data editing services

--- HydroServer/WaterOneFlow/WaterOneFlow_PHP/configuration/app_config.php
+++ HydroServer/WaterOneFlow/WaterOneFlow_PHP/configuration/app_config.php
@@ -4,5 +4,5 @@ define("DATABASE_HOST", "localhost");
 define("DATABASE_USERNAME", "advenup1_odm");
 define("DATABASE_PASSWORD", "MapW1nd0w");
 define("DATABASE_NAME", "advenup1_odm");
-define("SERVICE_CODE", "MOSS");
+define("SERVICE_CODE", "GHCN");
 define("LOWERCASE_TABLE_NAMES", "TRUE");
