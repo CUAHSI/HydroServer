cef95f6ffe36b70acc11aecc50d4f6d342b7e7a0
rohitkh_cp <SND\rohitkh_cp>
2012-06-01 22:42:55 +0000

    added the details page

--- HydroServer/WebClient_PHP/db_display_all.php
+++ HydroServer/WebClient_PHP/db_display_all.php
@@ -1,9 +1,5 @@
 <?php
-//Database Connection
-$username='advenup1_odm';
-$password='MapW1nd0w';
-$database='advenup1_odm';
-$db_connect='localhost';
+require_once 'db_config.php';
 
 // Start XML file, create parent node
 
@@ -11,17 +7,7 @@ $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
 $parnode = $dom->appendChild($node);
 
-// Opens a connection to a MySQL server
 
-$connection=mysql_connect ($db_connect, $username, $password);
-if (!$connection) {  die('Not connected : ' . mysql_error());}
-
-// Set the active MySQL database
-
-$db_selected = mysql_select_db($database, $connection);
-if (!$db_selected) {
-  die ('Can\'t use db : ' . mysql_error());
-}
 
 // Select all the rows in the markers table
 $dist=0;
@@ -37,13 +23,15 @@ header("Content-type: text/xml");
 while ($row = @mysql_fetch_assoc($result)){
   $node = $dom->createElement("marker");
   $newnode = $parnode->appendChild($node);
-  $newnode->setAttribute("name", $row['SiteID']);
-  $newnode->setAttribute("address", $row['SiteCode']);
+  $newnode->setAttribute("name", $row['SiteName']);
+  $newnode->setAttribute("siteid", $row['SiteID']);
+   $newnode->setAttribute("sitecode", $row['SiteCode']);
   $newnode->setAttribute("lat", $row['Latitude']);
   $newnode->setAttribute("lng", $row['Longitude']);
-  $newnode->setAttribute("type", $row['SiteType']);
+  $newnode->setAttribute("sitetype", $row['SiteType']);
   $newnode->setAttribute("distance", $dist);
 }
 
 echo $dom->saveXML();
+mysql_close($connect);
 ?>
--- HydroServer/WebClient_PHP/db_search.php
+++ HydroServer/WebClient_PHP/db_search.php
@@ -1,11 +1,6 @@
 <?php
 
-//Database Connection
-
-$username='advenup1_odm';
-$password='MapW1nd0w';
-$database='advenup1_odm';
-$db_connect='localhost';
+require_once 'db_config.php';
 
 // Get parameters from URL
 $center_lat = $_GET["lat"];
@@ -17,17 +12,7 @@ $dom = new DOMDocument("1.0");
 $node = $dom->createElement("markers");
 $parnode = $dom->appendChild($node);
 
-// Opens a connection to a mySQL server
-$connection=mysql_connect ($db_connect, $username, $password);
-if (!$connection) {
-  die("Not connected : " . mysql_error());
-}
 
-// Set the active mySQL database
-$db_selected = mysql_select_db($database, $connection);
-if (!$db_selected) {
-  die ("Can\'t use db : " . mysql_error());
-}
 
 // Search the rows in the markers table
 $query = sprintf("SELECT SiteID, SiteCode,SiteName, Latitude, Longitude, SiteType, ( 3959 * acos( cos( radians('%s') ) * cos( radians( Latitude ) ) * cos( radians( Longitude ) - radians('%s') ) + sin( radians('%s') ) * sin( radians( Latitude ) ) ) ) AS distance FROM sites HAVING distance < '%s' ORDER BY distance",
@@ -48,15 +33,17 @@ header("Content-type: text/xml");
 while ($row = @mysql_fetch_assoc($result)){
   $node = $dom->createElement("marker");
   $newnode = $parnode->appendChild($node);
-  $newnode->setAttribute("name", $row['SiteID']);
-  $newnode->setAttribute("address", $row['SiteCode']);
+  $newnode->setAttribute("name", $row['SiteName']);
+  $newnode->setAttribute("siteid", $row['SiteID']);
+  $newnode->setAttribute("sitecode", $row['SiteCode']);
   $newnode->setAttribute("lat", $row['Latitude']);
   $newnode->setAttribute("lng", $row['Longitude']);
   $newnode->setAttribute("distance", $row['distance']);
-  $newnode->setAttribute("type", $row['SiteType']);
+  $newnode->setAttribute("sitetype", $row['SiteType']);
 }
 
 //Output the XML DATA to be fed into the google maps api
 
 echo $dom->saveXML();
+mysql_close($connect);
 ?>
