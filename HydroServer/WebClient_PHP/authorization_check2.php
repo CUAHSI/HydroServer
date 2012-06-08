4f819d6bc9e5ee648c0a2a128442ef081310423d
rburch_cp <SND\rburch_cp>
2012-06-08 22:40:15 +0000

    moved the navigation menus in js tothe js folder

--- HydroServer/WebClient_PHP/authorization_check2.php
+++ HydroServer/WebClient_PHP/authorization_check2.php
@@ -2,16 +2,16 @@
 	
 //Display the correct navigation or redirect them to the unauthorized user page
 if ($_COOKIE[power] ==admin){
-	$nav ="<script src=A_navbar.js></script>";
+	$nav ="<script src=js/A_navbar.js></script>";
 	}
 elseif ($_COOKIE[power] ==teacher){
-	$nav ="<script src=T_navbar.js></script>";
+	$nav ="<script src=js/T_navbar.js></script>";
 	}
 elseif ($_COOKIE[power] ==student){
-	$nav ="<script src=S_navbar.js></script>";
+	$nav ="<script src=js/S_navbar.js></script>";
 	} 
 else {
-	$nav ="<script src=P_navbar.js></script>";
+	$nav ="<script src=js/P_navbar.js></script>";
 	}
 	
 ?>
