aad2ae4f5c17e239468fe6ef2f5ab5aa6aabcf64
rburch_cp <SND\rburch_cp>
2012-06-11 17:14:22 +0000

    deleted both files since they were moved to the js folder

--- HydroServer/WebClient_PHP/P_navbar.js
+++
@@ -1,1 +0,0 @@
-document.write("<blockquote><br /><strong>Public Navigation</strong><br /><p><strong>View Data Collected</strong><br /><a href=../view_main.php>Search Data</a></p><p><a href=../index.php>Back to Home</a></p></blockquote>");
