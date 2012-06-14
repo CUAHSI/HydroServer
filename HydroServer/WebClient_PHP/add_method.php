bdc1cab969c925c40e02f0e8bf4f9ecd83de7ab1
rburch_cp <SND\rburch_cp>
2012-06-14 18:16:28 +0000

    Updated the file to include a ListBox for selecting applicable Variable(s).

--- HydroServer/WebClient_PHP/add_method.php
+++ HydroServer/WebClient_PHP/add_method.php
@@ -15,7 +15,50 @@ if ($_COOKIE[power] !="admin"){
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>HydroServer Lite Web Client</title>
 <link href="styles/main_css.css" rel="stylesheet" type="text/css" media="screen" />
+<link rel="stylesheet" href="jqwidgets/styles/jqx.base.css" type="text/css" />
+<script type="text/javascript" src="scripts/gettheme.js"></script>
+<script type="text/javascript" src="scripts/jquery-1.7.2.min.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxcore.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxdata.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxbuttons.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxscrollbar.js"></script>
+<script type="text/javascript" src="jqwidgets/jqxlistbox.js"></script>
 
+<script type="text/javascript">
+	
+	//put together the Variable array			
+	var row_no=1;
+	var row_id=new Array;
+	row_id[0]="VariableID1";
+
+	var source =
+	{
+       	datatype: "json",
+	   	datafields: [
+          	{ name: 'variableid' },
+	        { name: 'variablename' },
+       	],
+           	url: 'db_get_types.php'
+	};				
+	
+	var dataAdapter = new $.jqx.dataAdapter(source);
+	
+    $(document).ready(function (){
+				
+		//Creating the list box
+		$("#VariableID1").jqxListBox(
+    		{
+			source: dataAdapter,
+		    theme: 'darkblue',
+			multiple: true,
+    	    width: 300,
+			height: 250,
+		    displayMember: 'variablename',
+       	    valueMember: 'variableid'
+			});
+	});
+
+</script>
 </head>
 
 <body background="images/bkgrdimage.jpg">
@@ -29,17 +72,17 @@ if ($_COOKIE[power] !="admin"){
   <tr>
     <td width="240" valign="top" bgcolor="#f2e6d6"><?php echo "$nav"; ?></td>
     <td width="720" valign="top" bgcolor="#FFFFFF"><blockquote><br />
-      <h1>Add a new Method to the database    </h1>
+      <h1>Add a new Method</h1>
       <p>&nbsp;</p>
       <FORM METHOD="POST" ACTION="do_add_method.php" name="addmethod">
         <table width="600" border="0" cellspacing="0" cellpadding="0">
         <tr>
-          <td width="94" valign="top"><strong>Method Name:</strong></td>
+          <td width="101" valign="top"><strong>Method Name:</strong></td>
           <td colspan="2" valign="top"><input type="text" id="MethodDescription" name="MethodDescription" size=20 maxlength=100"/>&nbsp;<span class="em">(Ex: YSI DO 200 Meter)</span></td>
         </tr>
         <tr>
-          <td width="94" valign="top">&nbsp;</td>
-          <td width="236" valign="top">&nbsp;</td>
+          <td width="101" valign="top">&nbsp;</td>
+          <td width="229" valign="top">&nbsp;</td>
           <td width="270" valign="top">&nbsp;</td>
         </tr>
         <tr>
@@ -48,16 +91,27 @@ if ($_COOKIE[power] !="admin"){
           </tr>
         <tr>
           <td valign="top">&nbsp;</td>
-          <td width="236" valign="top">&nbsp;</td>
+          <td valign="top">&nbsp;</td>
+          <td valign="top">&nbsp;</td>
+        </tr>
+        <tr>
+          <td colspan="3" valign="top"><strong>Variable(s) used by this method:</strong> <br>
+            (select all that apply by holding the &quot;Ctrl&quot; key down and selecting multiple options):</td>
+          </tr>
+        <tr>
+          <td colspan="3" valign="top">&nbsp;</td>
+        </tr>
+        <tr>
+          <td valign="top">&nbsp;</td>
+          <td colspan="2" valign="top"><div id="VariableID1"></div></td>
+          </tr>
+        <tr>
+          <td width="101" valign="top">&nbsp;</td>
+          <td width="229" valign="top">&nbsp;</td>
           <td width="270" valign="top">&nbsp;</td>
         </tr>
         <tr>
-          <td width="94" valign="top">&nbsp;</td>
-          <td width="236" valign="top">&nbsp;</td>
-          <td width="270" valign="top">&nbsp;</td>
-        </tr>
-        <tr>
-          <td colspan="2" valign="top"><center><input type="SUBMIT" name="submit" value="Add New Method" /></center></td>
+          <td colspan="2" valign="top"><center><input type="SUBMIT" name="submit" value="Add New Method" class="button"/></center></td>
           <td valign="top">&nbsp;</td>
           </tr>
       </table></FORM>
