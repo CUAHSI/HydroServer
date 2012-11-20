7629c8e8d349a4e13c2cfe4fc37f9ae7e68bc028
jpatton_cp <SND\jpatton_cp>
2012-11-20 23:28:07 +0000

    ODM_Tools Python -- Completed a few missing relationships.

--- HydroServer/ODM_Tools Python/odmdata/variable.py
+++ HydroServer/ODM_Tools Python/odmdata/variable.py
@@ -10,21 +10,19 @@ class Variable(Base):
 	code	  		  = Column('VariableCode', String, nullable=False)
 	name	  		  = Column('VariableName', String, nullable=False)
 	speciation 		  = Column('Speciation', String, nullable=False)
-	variable_units_id = Column('VariableUnitsID', Integer, ForeignKey('Units.UnitsID'), nullable=False)
+	variable_unit_id  = Column('VariableUnitsID', Integer, ForeignKey('Units.UnitsID'), nullable=False)
 	sample_medium	  = Column('SampleMedium', String, nullable=False)
 	value_type		  = Column('ValueType', String, nullable=False)
 	is_regular		  = Column('IsRegular', Boolean, nullable=False)
 	time_support	  = Column('TimeSupport', Float, nullable=False)
-	time_units_id	  = Column('TimeUnitsID', Integer, ForeignKey('Units.UnitsID'), nullable=False)
+	time_unit_id	  = Column('TimeUnitsID', Integer, ForeignKey('Units.UnitsID'), nullable=False)
 	data_type		  = Column('DataType', String, nullable=False)
 	general_category  = Column('GeneralCategory', String, nullable=False)
 	no_data_value	  = Column('NoDataValue', Float, nullable=False)
 
 	# relationships
-	#variable_unit = relationship("Units", 
-	#	primaryjoin="Units.UnitsID==Variables.VariableUnitsID")
-	#time_unit = relationship("Units", 
-	#	primaryjoin="Units.UnitsID==Variables.TimeUnitsID")
+	variable_unit = relationship(Unit, primaryjoin=("Unit.id==Variable.variable_unit_id"))		# <-- Uses class attribute names, not table column names
+	time_unit 	  = relationship(Unit, primaryjoin=("Unit.id==Variable.time_unit_id"))
 
 	def __repr__(self):
 		return "<Variable('%s', '%s', '%s')>" % (self.id, self.code, self.name)
