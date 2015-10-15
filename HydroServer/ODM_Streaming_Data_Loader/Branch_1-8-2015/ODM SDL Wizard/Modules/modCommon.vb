c2c70c0466ad05b1b7874f611ac20fc6189b2822
sreeder_cp <SND\sreeder_cp>
2015-10-15 20:03:19 +0000

    update Version numbers for sdl

--- HydroServer/ODM_Streaming_Data_Loader/Branch_1-8-2015/ODM SDL Wizard/Modules/modCommon.vb
+++ HydroServer/ODM_Streaming_Data_Loader/Branch_1-8-2015/ODM SDL Wizard/Modules/modCommon.vb
@@ -122,7 +122,7 @@ Module Common
         Dim g_Config_Dir As String
         tempdir = System.IO.Path.GetDirectoryName(System.Configuration.ConfigurationManager.OpenExeConfiguration(System.Configuration.ConfigurationUserLevel.PerUserRoamingAndLocal).FilePath)
         Dim section As String() = Split(tempdir, "Configuration", , CompareMethod.Text)
-        g_Config_Dir = section(0) & "StreamingDataLoader\1.2.1\"
+        g_Config_Dir = section(0) & "StreamingDataLoader\1.2.1.1\"
         IO.Directory.CreateDirectory(g_Config_Dir)
         Return g_Config_Dir
 
